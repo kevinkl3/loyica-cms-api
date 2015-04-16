a919cbe5c0257907c3a54f58b281b5a22851cbde
Mijail Paz <mijail@gmail.com>
2015-04-15 18:23:32 -0600

    Typo en ServiceController

--- protected/controllers/ServiceController.php
+++ protected/controllers/ServiceController.php
@@ -1,5 +1,5 @@
 <?php
-class ServicesController extends BaseController{
+class ServiceController extends BaseController{
 	protected function beforeAction( $action){
 		$this->mModel = Service::model();
 		return parent::beforeAction($action);
