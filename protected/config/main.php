566be716154028262cf666367a7c693d872ac40f
kevinkl3 <kevinlopez@unitec.edu>
2015-10-05 22:40:44 -0600

    Made controllers able to be plural, eg: places, zones,products

--- protected/config/main.php
+++ protected/config/main.php
@@ -67,6 +67,7 @@ return array(
 				//'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				'<controller:\w+>s'=>'<controller>/index',
 			),
 		),
 		'db'=>array(
